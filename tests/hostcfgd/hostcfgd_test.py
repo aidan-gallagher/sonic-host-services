d6c1d6c4b4b99308807ef2931576178d5d043160
Aidan Gallagher <aidan@cloudflare.com>
2024-09-18 17:09:31 +0100

    Local-login: Listen to CFG_LOCAL_LOGIN_TABLE_NAME
    When the local-login password changes restart the
    local-login systemd service.

--- tests/hostcfgd/hostcfgd_test.py
+++ tests/hostcfgd/hostcfgd_test.py
@@ -312,3 +312,19 @@ class TestDnsHandler:
         data = {}
         dns_cfg.load(data)
         dns_cfg.dns_update.assert_called()
+
+class TestLocalLoginCfg:
+    def test_load(self):
+        local_login_cfg = hostcfgd.LocalLoginCfg()
+        local_login_cfg.local_login_update = mock.MagicMock()
+
+        data = {}
+        local_login_cfg.load(data)
+        local_login_cfg.local_login_update.assert_called()
+
+    @mock.patch('hostcfgd.run_cmd')
+    def test_local_login_update(self, mock_run_cmd):
+        local_login_cfg = hostcfgd.LocalLoginCfg()
+        local_login_cfg.local_login_update(None, None, None)
+
+        mock_run_cmd.assert_has_calls([call(['sudo', 'systemctl', 'restart', 'local-login'])])
